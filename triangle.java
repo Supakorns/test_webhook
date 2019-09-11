d27db0153a445f2251bf58ab48aa2c3eba866822
Supakorns <tonytonychopss@gmail.com>
2019-09-11 18:11:11 +0700

    second time

---
+++ triangle.java
@@ -0,0 +1,12 @@
+class triangle {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+	for(int i = 0; i < 5 ; i++){
+		for(int j = 0; j < i ; j++){
+			System.out.print(5);
+		}System.out.println();
+	}
+	System.out.println("test 123456");
+
+    }
+}
